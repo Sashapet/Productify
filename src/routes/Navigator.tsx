e277616446af2ca5c5561c41b5415f648c92a53f
Sashapet <fastzumftwlols@gmail.com>
2021-10-23 14:40:40 +0300

    feat: :art: BootSplash

--- src/routes/Navigator.tsx
+++ src/routes/Navigator.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useTranslation } from 'react-i18next';
 import { lightTheme } from '@assets/theme/theme';
 import { ThemeProvider } from 'styled-components/native';
-
+import RNBootSplash from 'react-native-bootsplash';
 import { LandingView } from '../containers/LoginFlow';
 
 const Navigator = () => {
@@ -12,9 +13,14 @@ const Navigator = () => {
 
   const Stack = createStackNavigator();
 
+  //hide splash
+  const hideSplash = useCallback(() => {
+    RNBootSplash.hide({ fade: true });
+  }, []);
+
   return (
     <ThemeProvider theme={lightTheme}>
-      <NavigationContainer>
+      <NavigationContainer onReady={hideSplash}>
         <Stack.Navigator headerMode="none">
           <Stack.Screen name="LandingScreen" component={LandingView} />
         </Stack.Navigator>
